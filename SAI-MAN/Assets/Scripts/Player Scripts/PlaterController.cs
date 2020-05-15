dd7f2df190ac21dcbb8f4ad386a1bf72551d8751
hemunt <hemuntcoc@gmail.com>
2020-05-15 19:09:35 +0530

    Player And Movement
    I added Player and its movement in this version. and did some minor changer (Project Working well no issue)

---
+++ SAI-MAN/Assets/Scripts/Player Scripts/PlaterController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaterController : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float movespeed = 30f;
+    float horizontalMove = 0f;
+    bool jump = false;
+   // public Animator anim;
+    private Transform tr;
+
+
+    private void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * movespeed;
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+            //anim.SetTrigger("Takeoff");
+        }
+        if (horizontalMove == 0)
+        {
+            //anim.SetBool("Running", false);
+
+        }
+        else if (horizontalMove > 0)
+        {
+            
+           // anim.SetBool("Running", true);
+         
+        }
+        else if (horizontalMove < 0)
+        {
+          
+            //anim.SetBool("Running", true);
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.deltaTime, false, jump);
+        jump = false;
+    }
+}
