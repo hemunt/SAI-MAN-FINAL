9b31662c05a0845323f01a2538663fb8243fe050
hemunt <hemuntcoc@gmail.com>
2020-05-19 20:57:59 +0530

    Spikes and map
    Added spicks and added map and camera move

---
+++ SAI-MAN/Assets/Scripts/Spicks/Spicks.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spicks : MonoBehaviour
+{
+
+
+    private BoxCollider2D collider;
+    public float Up;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        collider = gameObject.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+      
+
+            Vector3 spikes = transform.position;
+            spikes.y += Up;
+            transform.position = spikes;
+            collider.enabled = false;
+      
+    }
+}
