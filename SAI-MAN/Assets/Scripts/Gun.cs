93239ff3a2935457853cfec157b4e9e011481669
hemunt <hemuntcoc@gmail.com>
2020-05-23 21:36:26 +0530

    Tring Shooting

---
+++ SAI-MAN/Assets/Scripts/Gun.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float offSet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 diff = Camera.main.ScreenToViewportPoint(Input.mousePosition) - transform.position;
+        float rotz = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotz + offSet);
+    }
+}
