fb33734f3959ec8ed8d445d42c4d02d34f0a5c0b
hemunt <hemuntcoc@gmail.com>
2020-05-25 17:50:15 +0530

    Character rigging Done
    2d Character bones and all the Target settings and  joints are done here

--- SAI-MAN/Assets/Scripts/Gun.cs
+++ SAI-MAN/Assets/Scripts/Gun.cs
@@ -9,7 +9,7 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 diff = Camera.main.ScreenToViewportPoint(Input.mousePosition) - transform.position;
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotz = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotz + offSet);
     }
