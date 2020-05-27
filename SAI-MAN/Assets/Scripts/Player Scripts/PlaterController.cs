ba152778c5bc6b8a5856e03875ee6aee4c9fd079
hemunt <hemuntcoc@gmail.com>
2020-05-27 21:16:26 +0530

    Just Added Enemy

--- SAI-MAN/Assets/Scripts/Player Scripts/PlaterController.cs
+++ SAI-MAN/Assets/Scripts/Player Scripts/PlaterController.cs
@@ -11,10 +11,22 @@ public class PlaterController : MonoBehaviour
     bool jump = false;
     public Animator anim;
     private Transform tr;
+    public GameObject ShootUp;
+    public GameObject ShootDown;
+    public GameObject Bullet;
+    public Transform GunPointLeft;
+    public Transform GunPointRight;
+    public GameObject AimIcone;
+  
 
+    private float timeBtwShots;
+    public float StartTimeBtwShots;
 
+   
     private void Start()
     {
+        
+        AimIcone.SetActive(false);
         tr = GetComponent<Transform>();
     }
     // Update is called once per frame
@@ -32,16 +44,73 @@ public class PlaterController : MonoBehaviour
         }
         else if (horizontalMove > 0)
         {
+
+            anim.SetBool("Running", true);
             
-           anim.SetBool("Running", true);
-         
         }
         else if (horizontalMove < 0)
         {
-          
+
             anim.SetBool("Running", true);
         }
-        
+
+        if (Input.GetMouseButton(1))
+        {
+            Vector2 UpPossition = ShootUp.transform.position;
+            Vector2 DownPossition = ShootDown.transform.position;
+            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            anim.SetBool("Aiming", true);
+            AimIcone.SetActive(true);
+            AimIcone.transform.position = difference;
+            
+            if (difference.y > UpPossition.y)
+            {
+                anim.SetBool("isAimingDown", false);
+                anim.SetBool("isAimingMid", false);
+                anim.SetBool("isAimingUp", true);
+            }
+
+            if (difference.y < UpPossition.y && difference.y > DownPossition.y)
+            {
+                anim.SetBool("isAimingDown", false);
+                anim.SetBool("isAimingMid", true);
+                anim.SetBool("isAimingUp", false);
+            }
+
+            if (difference.y < DownPossition.y)
+            {
+                anim.SetBool("isAimingDown", true);
+                anim.SetBool("isAimingMid", false);
+                anim.SetBool("isAimingUp", false);
+            }
+            if (timeBtwShots <= 0)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                   
+            
+                    Instantiate(Bullet, GunPointLeft.position, GunPointLeft.rotation);
+                    Instantiate(Bullet, GunPointRight.position, GunPointRight.rotation);
+
+                    timeBtwShots = StartTimeBtwShots;
+                }
+                else
+                {
+                }
+            }
+            else
+            {
+                timeBtwShots -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            AimIcone.SetActive(false);
+            
+            anim.SetBool("Aiming", false);
+        }
+
+
     }
     private void FixedUpdate()
     {
---
+++ SAI-MAN/Assets/Sprites/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float Speed;
+    public float lifeTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        transform.position = Vector3.MoveTowards(transform.position, difference, Speed * Time.deltaTime);
+    }
+    void DestroyProjectile() {
+        Destroy(gameObject);
+    }
+}
